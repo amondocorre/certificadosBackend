6fc55c0a0c9574743c8eba88ea88938ac434ac39
amondocorre <amondocorre@capressocafe.com>
2025-12-11 12:29:51 -0400

    implementacion de Certificado Psicologico

--- application/views/impresion/infPvaluacionPsicologica.php
+++ application/views/impresion/infPvaluacionPsicologica.php
@@ -35,143 +35,264 @@ $pdf->AddPage();
   $logoWidth = 26;
   $logoX = $pdf->GetX();
   $logoY = $pdf->GetY();
+  //imagen
+   //imagen
+  $rutaImagen =  $data->foto;
+  $nombreArchivo = basename($data->foto); // => "6.jpg"
+  // Ruta física completa en tu servidor
+  $rutaImagen = FCPATH . "assets/inv_evaluacion_psicologico/" . $nombreArchivo;
+  // Área máxima permitida (en mm: 1 cm = 10 mm)
+  $maxWidth = 25;  // 3 cm
+  $maxHeight = 30; // 2.5 cm
 
+   if (file_exists($rutaImagen)) {
+      // Obtener tamaño real de la imagen (en píxeles)
+      list($width, $height) = getimagesize($rutaImagen);
+      // Calcular proporción
+      $xRatio = $maxWidth / $width;
+      $yRatio = $maxHeight / $height;
+      // Escala proporcional (usa el factor menor)
+      $scale = min($xRatio, $yRatio);
+      $newWidth = $width * $scale;
+      $newHeight = $height * $scale;
+      // Dibujar imagen con tamaño ajustado
+      $pdf->Image($rutaImagen, $logoX + 140, $logoY + 40, $newWidth, $newHeight, '', '', false, 300);
+    } else {
+        $pdf->Cell(0, 5, "Imagen no encontrada: " . $rutaImagen, 0, 1, 'L');
+    }
   //$rutaImagen =  $data->foto;
   //$pdf->Image($rutaImagen, $logoX+140, $logoY+15, $logoWidth, '0', 'PNG');
-  $pdf->SetFont('helvetica', 'B', 14);
-  $afterLogoY = $logoY + $logoWidth + 2;
-  $pdf->SetXY($pdf->getMargins()['left']-10,$afterLogoY-12);
+  //$pdf->SetFont('helvetica', 'B', 14);
+  //$afterLogoY = $logoY + $logoWidth + 2;
+  //$pdf->SetXY($pdf->getMargins()['left']-10,$afterLogoY-12);
     
   //nombre completo
   $pdf->SetFont('helvetica', 'N', 10);
-  $pdf->SetXY($pdf->GetX()+8, $pdf->GetY()+15); // asegura posición
-  $pdf->Cell(33, 5, $data->ap_paterno, $margen, 0, 'C');
+  $pdf->SetXY($pdf->GetX()-5, $pdf->GetY()+35); // asegura posición
+  $pdf->Cell(35, 5, $data->ap_paterno, $margen, 0, 'C');
   $pdf->Cell(3, 5, "", $margen, 0, 'C');
   $pdf->Cell(35, 5, $data->ap_materno, $margen, 0, 'C');
-  $pdf->Cell(5, 5, "", $margen, 0, 'C');
-  $pdf->Cell(40, 5, $data->nombre, $margen, 0, 'C');
+  $pdf->Cell(1, 5, "", $margen, 0, 'C');
+  $pdf->Cell(35, 5, $data->nombre, $margen, 0, 'C');
   //edad
   //ci
-  $pdf->Cell(8, 5, "", $margen, 0, 'C');
-  $pdf->Cell(30, 5, $data->ci, $margen, 1, 'C');
-  $pdf->SetXY($pdf->GetX()-3, $pdf->GetY()+12); // asegura posición
+  $pdf->Cell(3, 5, "", $margen, 0, 'C');
+  $pdf->Cell(28, 5, $data->ci, $margen, 1, 'C');
+  $pdf->SetXY($pdf->GetX()-4, $pdf->GetY()+12); // asegura posición
   //fecha
   $fecha = date("d/m/Y", strtotime($data->fecha_nacimiento));
   $pdf->Cell(48, 5, $data->lugar_nacimiento.' '.$fecha, $margen, 0, 'L');
   //profesion
-  $pdf->Cell(8, 5, "", $margen, 0, 'C');
-  $pdf->Cell(45, 5, $data->profecion, $margen, 0, 'r');
-  //fecha examen
-  //$fechaExamen = date("d/m/Y", strtotime($data->fecha_evaluacion));
-  //$pdf->Cell(8, 5, "", $margen, 0, 'C');
-  //$pdf->Cell(40, 5, $fechaExamen, $margen, 1, 'r');
+  $pdf->Cell(20, 5, "", $margen, 0, 'C');
+  $pdf->Cell(45, 5, $data->profecion, $margen, 1, 'L');
   //DOMICILIO
-  $pdf->SetXY($pdf->GetX()-3, $pdf->GetY()+12); // asegura posición
-  $pdf->Cell(45, 5, $data->domicilio, $margen, 0, 'r');
+  $pdf->SetXY($pdf->GetX()-3, $pdf->GetY()+8); // asegura posición
+  $pdf->Cell(40, 5, $data->domicilio, $margen, 0, 'r');
   // nro
-  $pdf->Cell(3, 5, "", $margen, 0, 'C');
-  $pdf->Cell(17, 5, $data->numero_domicilio, $margen, 0, 'C');
+  $pdf->Cell(1, 5, "", $margen, 0, 'C');
+  $pdf->Cell(13, 5, $data->numero_domicilio, $margen, 0, 'C');
   // zona
-  $pdf->Cell(3, 5, "", $margen, 0, 'C');
-  $pdf->Cell(40, 5, $data->zona, $margen, 0, 'C');
+  $pdf->Cell(8, 5, "", $margen, 0, 'C');
+  $pdf->Cell(35, 5, $data->zona, $margen, 0, 'C');
   // telefono
   $pdf->Cell(2, 5, "", $margen, 0, 'C');
   $pdf->Cell(28, 5, $data->telefono, $margen, 1, 'C');
-  // HISTORIAL FAMILIAR
-  $pdf->SetFont('helvetica', 'N', 8);
-  $pdf->SetXY($pdf->GetX()+2, $pdf->GetY()+10); // asegura posición
-  $pdf->Cell(35, 5, "", $margen, 0, 'C');
-  $texto='SE PRESENTA A CONSULTA SUJETO DE '.$data->historia_familiar.'  AÑOS DE EDAD SIN ANTECEDENTES   PSICOLOGICOS PERSONALES O FAMILIARES DESTACABLES';
-  $pdf->MultiCell(138, 5, $texto, $margen, 'L');
-   // examen psicologico
-  /*
-   $pdf->SetXY($pdf->GetX(), $pdf->GetY()+3); // asegura posición
-  $pdf->Cell(38, 5, "", $margen, 0, 'C');
-  $pdf->MultiCell(133, 5, $data->historial_familiar, $margen, 'L');
-  */
-  //coordinacion visomotora
+  // HISTORIAL medico
   $pdf->SetFont('helvetica', 'N', 10);
-  $pdf->SetXY($pdf->GetX()+4, $pdf->GetY()+25); // asegura posición
-  switch (trim($data->coordinacion_visomotora??'')) {
+  $pdf->SetXY($pdf->GetX()-2, $pdf->GetY()+19); // asegura posición
+  $pdf->MultiCell(155, 15, $data->historia_medica, $margen, 'L');
+  // HISTORIAL FAMILIAR
+  $pdf->SetFont('helvetica', 'N', 10);
+  $pdf->SetXY($pdf->GetX()-2, $pdf->GetY()+12); // asegura posición
+  $pdf->MultiCell(155, 15, $data->historia_familiar, $margen, 'L');
+  //niveles de estres
+  $pdf->SetFont('helvetica', 'N', 10);
+  $pdf->SetXY($pdf->GetX()+2, $pdf->GetY()+59); // asegura posición
+  switch (trim($data->niveles_estres??'')) {
     case 'adecuado':
-        $pdf->Cell(35, 5, "", $margen, 0, 'C');
-        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
-        break;
-    case 'inadecuado':
-        $pdf->Cell(80, 5, "", $margen, 0, 'C');
-        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
-        break;
-    case 'observacion':
-        $pdf->Cell(125, 5, "", $margen, 0, 'C');
-        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
-        break;
-    
-  }
-  //PERSONALIDAD
-  $pdf->SetXY($pdf->GetX()+4, $pdf->GetY()+17); // asegura posición
-  switch (trim($data->personalidad??'')) {
-    case 'adecuado':
-        $pdf->Cell(35, 5, "", $margen, 0, 'C');
-        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
-        break;
-    case 'inadecuado':
-        $pdf->Cell(80, 5, "", $margen, 0, 'C');
-        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
-        break;
-    case 'observacion':
-        $pdf->Cell(125, 5, "", $margen, 0, 'C');
-        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
-        break;
-    
-  }
-  //memoria
-  $pdf->SetXY($pdf->GetX()+4, $pdf->GetY()+20); // asegura posición
-  switch (trim($data->atencion_cognitiva??'')) {
-    case 'adecuado':
-        $pdf->Cell(35, 5, "", $margen, 0, 'C');
-        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
-        break;
-    case 'inadecuado':
-        $pdf->Cell(80, 5, "", $margen, 0, 'C');
-        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
-        break;
-    case 'opservacion':
-        $pdf->Cell(125, 5, "", $margen, 0, 'C');
-        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
-        break;
-    
-  }
-  //estres
-  $pdf->SetXY($pdf->GetX()+4, $pdf->GetY()+25); // asegura posición
-  switch (trim($data->reaccion_estres_riego??'')) {
-    case 'optio':
-        $pdf->Cell(33, 5, "", $margen, 0, 'C');
+        $pdf->Cell(10, 5, "", $margen, 0, 'C');
         $pdf->Cell(8, 5, "X", $margen, 1, 'C');
         break;
     case 'medio':
-        $pdf->Cell(75, 5, "", $margen, 0, 'C');
+        $pdf->Cell(50, 5, "", $margen, 0, 'C');
         $pdf->Cell(8, 5, "X", $margen, 1, 'C');
         break;
     case 'inadecuado':
-        $pdf->Cell(125, 5, "", $margen, 0, 'C');
+        $pdf->Cell(101, 5, "", $margen, 0, 'C');
         $pdf->Cell(8, 5, "X", $margen, 1, 'C');
         break;
-    
+    case 'observación':
+        $pdf->Cell(151, 5, "", $margen, 0, 'C');
+        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
+        break;
+  }
+  
+  //niveles de estres
+  $pdf->SetFont('helvetica', 'N', 10);
+  $pdf->SetXY($pdf->GetX()+2, $pdf->GetY()+16); // asegura posición
+  switch (trim($data->estrategias_afrontamiento??'')) {
+    case 'adecuado':
+        $pdf->Cell(10, 5, "", $margen, 0, 'C');
+        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
+        break;
+    case 'medio':
+        $pdf->Cell(50, 5, "", $margen, 0, 'C');
+        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
+        break;
+    case 'inadecuado':
+        $pdf->Cell(101, 5, "", $margen, 0, 'C');
+        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
+        break;
+    case 'observación':
+        $pdf->Cell(151, 5, "", $margen, 0, 'C');
+        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
+        break;
   }
 
-   // observaciones
-  $pdf->SetFont('helvetica', 'N', 6);
-  $pdf->SetXY($pdf->GetX()+2, $pdf->GetY()+8); // asegura posición
-  $pdf->Cell(30, 5, "", $margen, 0, 'C');
-  $pdf->MultiCell(133, 5, '(EN ESTE ACAPITE SE DEBE INCORPORAR SI EL POSTULANTE ES APTO, SI NO FUERA APTO INDICAR LOS MOTIVOS)', $margen, 'L');
   
-   // observaciones
-  $pdf->SetFont('helvetica', 'B', 7);
-  $pdf->SetXY($pdf->GetX(), $pdf->GetY()+1); // asegura posición
-  //$pdf->MultiCell(165, 5, $data->observacion, $margen, 'L');
-  //$pdf->MultiCell(165, 5, 'DEL PRESENTE APTO DE EVALUACION PSICOLOGICA DE ACUERDO A LOS RESULTADOS  OBTENIDOS DE LAS PRUEBAS APLICADAS CONCLUYO QUE EL INTERESADO, NO PRESENTA ALTERACIONES FUNCIONALES SIGNIFICATIVOS O DISMINUIDAS. EL PRESENTE OSTENTA CAPACIDADES ADECUADAS EN LO QUE CONCLUYO ES APTO PARA CONDUCIR VEHICULOS ', $margen, 'L');
-  $pdf->MultiCell(165, 5, 'DEL PRESENTE APTO DE EVALUACION PSICOLOGICA DE ACUERDO A LOS RESULTADOS  OBTENIDOS DE LAS PRUEBAS APLICADAS CONCLUYO QUE EL INTERESADO, NO PRESENTA ALTERACIONES FUNCIONALES SIGNIFICATIVOS O DISMINUIDAS. EL PRESENTE OSTENTA CAPACIDADES ADECUADAS EN LO QUE CONCLUYO ES APTO PARA CONDUCIR VEHICULOS ', $margen, 'L');
+  //vulnerabilidad_emocional
+  $pdf->SetFont('helvetica', 'N', 10);
+  $pdf->SetXY($pdf->GetX()+2, $pdf->GetY()+21); // asegura posición
+  switch (trim($data->vulnerabilidad_emocional??'')) {
+    case 'adecuado':
+        $pdf->Cell(10, 5, "", $margen, 0, 'C');
+        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
+        break;
+    case 'medio':
+        $pdf->Cell(50, 5, "", $margen, 0, 'C');
+        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
+        break;
+    case 'inadecuado':
+        $pdf->Cell(101, 5, "", $margen, 0, 'C');
+        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
+        break;
+    case 'observación':
+        $pdf->Cell(151, 5, "", $margen, 0, 'C');
+        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
+        break;
+  }
+
+  $pdf->AddPage();
+  $pdf->SetFont('helvetica', 'N', 10);
   
+  //atencion_sostenida_selectiva
+  $pdf->SetFont('helvetica', 'N', 8);
+  $pdf->SetXY($pdf->GetX()+5, $pdf->GetY()+25); // asegura posición
+  switch (trim($data->atencion_sostenida_selectiva??'')) {
+    case 'adecuado':
+        $pdf->Cell(11, 5, "", $margen, 0, 'C');
+        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
+        break;
+    case 'medio':
+        $pdf->Cell(52, 5, "", $margen, 0, 'C');
+        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
+        break;
+    case 'inadecuado':
+        $pdf->Cell(104, 5, "", $margen, 0, 'C');
+        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
+        break;
+    case 'observación':
+        $pdf->Cell(152, 5, "", $margen, 0, 'C');
+        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
+        break;
+  }
+
+  //capacidad_reaccion
+  $pdf->SetFont('helvetica', 'N', 8);
+  $pdf->SetXY($pdf->GetX()+5, $pdf->GetY()+21); // asegura posición
+  switch (trim($data->capacidad_reaccion??'')) {
+    case 'adecuado':
+        $pdf->Cell(11, 5, "", $margen, 0, 'C');
+        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
+        break;
+    case 'medio':
+        $pdf->Cell(52, 5, "", $margen, 0, 'C');
+        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
+        break;
+    case 'inadecuado':
+        $pdf->Cell(104, 5, "", $margen, 0, 'C');
+        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
+        break;
+    case 'observación':
+        $pdf->Cell(152, 5, "", $margen, 0, 'C');
+        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
+        break;
+  }
+
+  //control_impulso:                    string;
+  $pdf->SetFont('helvetica', 'N', 8);
+  $pdf->SetXY($pdf->GetX()+5, $pdf->GetY()+16); // asegura posición
+  switch (trim($data->control_impulso??'')) {
+    case 'adecuado':
+        $pdf->Cell(11, 5, "", $margen, 0, 'C');
+        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
+        break;
+    case 'medio':
+        $pdf->Cell(52, 5, "", $margen, 0, 'C');
+        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
+        break;
+    case 'inadecuado':
+        $pdf->Cell(104, 5, "", $margen, 0, 'C');
+        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
+        break;
+    case 'observación':
+        $pdf->Cell(152, 5, "", $margen, 0, 'C');
+        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
+        break;
+  }
+
+  //actidud_norma_autoridad
+  $pdf->SetFont('helvetica', 'N', 8);
+  $pdf->SetXY($pdf->GetX()+5, $pdf->GetY()+17); // asegura posición
+  switch (trim($data->actidud_norma_autoridad??'')) {
+    case 'adecuado':
+        $pdf->Cell(11, 5, "", $margen, 0, 'C');
+        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
+        break;
+    case 'medio':
+        $pdf->Cell(52, 5, "", $margen, 0, 'C');
+        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
+        break;
+    case 'inadecuado':
+        $pdf->Cell(104, 5, "", $margen, 0, 'C');
+        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
+        break;
+    case 'observación':
+        $pdf->Cell(152, 5, "", $margen, 0, 'C');
+        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
+        break;
+  }
+
+  //capacidad_reaccion
+  $pdf->SetFont('helvetica', 'N', 8);
+  $pdf->SetXY($pdf->GetX()+5, $pdf->GetY()+16); // asegura posición
+  switch (trim($data->estabilidad_emocional??'')) {
+    case 'adecuado':
+        $pdf->Cell(11, 5, "", $margen, 0, 'C');
+        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
+        break;
+    case 'medio':
+        $pdf->Cell(52, 5, "", $margen, 0, 'C');
+        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
+        break;
+    case 'inadecuado':
+        $pdf->Cell(104, 5, "", $margen, 0, 'C');
+        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
+        break;
+    case 'observación':
+        $pdf->Cell(152, 5, "", $margen, 0, 'C');
+        $pdf->Cell(8, 5, "X", $margen, 1, 'C');
+        break;
+  }
+  // Hresultado_recomendacion
+  $pdf->SetFont('helvetica', 'N', 10);
+  $pdf->SetXY($pdf->GetX()-2, $pdf->GetY()+13); // asegura posición
+  $pdf->MultiCell(165, 15, $data->resultado_recomendacion, $margen, 'L');
+  // observacion
+  $pdf->SetFont('helvetica', 'N', 9);
+  $pdf->SetXY($pdf->GetX()-2, $pdf->GetY()+10); // asegura posición
+  $pdf->MultiCell(165, 15, $data->observacion, $margen, 'L');
+
 
 
 
